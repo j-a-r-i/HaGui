58677d8ffcb4ce1e19709964bde676e387108515
j-a-r-i <jojanen@elisanet.fi>
2015-11-16 21:32:31 +0200

    IntervalAction added and Scheduler uses timer

--- scheduler.js
+++ scheduler.js
@@ -1,11 +1,22 @@
 "use strict";
 
+//-----------------------------------------------------------------------------
 function clock(hour,min)
 {
 	return hour*60 + min;
 }
 
-class SimpleAction {
+//-----------------------------------------------------------------------------
+function toStr(clock)
+{
+	var hours = Math.ceil(clock / 60);
+	var mins = clock % 60;
+	
+	return "" + hours + ":" + mins;
+}
+
+//-----------------------------------------------------------------------------
+class RangeAction {
 	constructor(astart, astop)
 	{
 		this._start = astart;
@@ -15,13 +26,21 @@ class SimpleAction {
 	
 	tick(clock)
 	{
-		if (!this._active && clock >= this._start) {
-			console.log("start");
-			this._active = true;
+		if (clock < this._start) {
+			// do nothing
 		}
-		if (this._active && clock > this._stop) {
-			console.log("stop");
-			this._active = false;
+		else if (clock < this._stop) {
+			if (!this._active) {
+				console.log("start");
+				this._active = true;
+			}	
+		}
+		else {
+			if (this._active) {
+				console.log("stop");
+				this._active = false;
+			}
+			
 		}
 	}
 	
@@ -31,10 +50,40 @@ class SimpleAction {
 	}
 }
 
+//-----------------------------------------------------------------------------
+class IntervalAction {
+	constructor(interval, func)
+	{
+		this._interval = interval;
+		this._callback = func;
+		this._started = 0 + this._interval;
+	}
+	
+	tick(clock)
+	{
+		if (clock >= this._started) {
+			this._callback();
+			this._started = clock + this._interval;			
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
 class Scheduler {
 	constructor()
 	{
 		this._actions = [];
+		this._timer = null;
+	}
+	
+	start()
+	{
+		this._timer = setInterval(this.onTimer, 60000, this);  // 1 min interval
+	}
+	
+	stop()
+	{
+		clearInterval(this._timer);
 	}
 	
 	add(action)
@@ -42,17 +91,28 @@ class Scheduler {
 		this._actions.push(action);
 	}
 	
-	tick()
+	tick(clock)
 	{
-		var c = clock(10,20);
 		this._actions.forEach(function(act) {
-			act.tick(c);
+			act.tick(clock);
 		});
 	}
+
+	onTimer(self)
+	{
+		var d = new Date();
+		var c = clock(d.getHours(), d.getMinutes());
+		//console.log("<timer2>");
+		self.tick(c);
+	}
+	
 }
 
+//-----------------------------------------------------------------------------
 module.exports = {
-	clock: clock,
-	SimpleAction: SimpleAction,
+	toClock: clock,
+	toStr: toStr,
+	RangeAction: RangeAction,
+	IntervalAction: IntervalAction,
 	Scheduler: Scheduler
 };
