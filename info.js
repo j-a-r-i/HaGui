6935b823304fdeeaaaf0434839205f85e4291eca
j-a-r-i <jojanen@elisanet.fi>
2015-11-18 19:53:00 +0200

    info: meminfo and loadavg implementation

--- info.js
+++ info.js
@@ -1,30 +1,48 @@
 // https://nodejs.org/api/child_process.html
 //
-var exec = require('child_process').exec;
+var exec = require('child_process').exec,
+    fs   = require('fs')
 
+const MEMINFO = '/proc/meminfo';
+const LOADAVG = '/proc/loadavg';
 
-var c1 = exec("df", function (error, stdout, stderr) {
-    if (error !== null) {
-        console.log('ERROR: ' + error);
-        return;
-    }
-    console.log('stdout: ' + stdout);
-});
+//-----------------------------------------------------------------------------
+function df()
+{
+    var c1 = exec("df", function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log('ERROR: ' + error);
+            return;
+        }
+        console.log('stdout: ' + stdout);
+    });
+}
 
+//-----------------------------------------------------------------------------
+function meminfo()
+{
+    var minfo = {};
+    var data = fs.readFileSync(MEMINFO).toString().trim();
+    data.split("\n").forEach(function (line) {
+        var arr = line.split(':');
+        arr[0] = arr[0].replace('(', '_');
+        arr[0] = arr[0].replace(')', '');
+        if (arr.length === 2) {
+            minfo[arr[0]] = parseInt(arr[1]);
+        }
+    });
+    return minfo;
+}
 
-var c2 = exec("ps", function (error, stdout, stderr) {
-    if (error !== null) {
-        console.log('ERROR: ' + error);
-        return;
-    }
-    console.log('stdout: ' + stdout);
-});
+//-----------------------------------------------------------------------------
+function loadavg()
+{
+    var line = fs.readFileSync(LOADAVG).toString();
+    return line.trim().split(" ");
+}
 
-
-var c3 = exec("free", function (error, stdout, stderr) {
-    if (error !== null) {
-        console.log('ERROR: ' + error);
-        return;
-    }
-    console.log('stdout: ' + stdout);
-});
+//-----------------------------------------------------------------------------
+module.exports = {
+	meminfo: meminfo,
+	loadavg: loadavg
+};
