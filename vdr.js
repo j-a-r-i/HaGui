87ffb9b444a040c25afc75d55f38e40a56122690
j-a-r-i <jojanen@elisanet.fi>
2016-04-11 19:35:12 +0300

    vdr: initial testing for reading EPG data

---
+++ vdr.js
@@ -0,0 +1,25 @@
+var net = require('net'),
+    rl  = require('readline'),
+    config = require('./config.json'),
+
+var PORT = 6419,
+    HOST = config.vdrServer;
+    
+var socket = net.createConnection(PORT);
+
+console.log('Socket created.');
+//socket.on('data', function(data) {
+//    // Log the response from the HTTP server.
+//    //var arr = data.split('\n');
+//    console.log(data.length);
+//})
+socket.on('connect', () => {
+    // Manually write an HTTP request.
+    socket.write("LSTE\r\n");
+    var reader = rl.createInterface({input: socket});
+    reader.on('line', (line) => {
+	console.log("<" + line.substring(4) + ">");
+    });
+//}).on('end', function() {
+//  console.log('DONE');
+});
